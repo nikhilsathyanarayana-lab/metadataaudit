63e701b1413172c2f9b8f0b8f27da41ab6220aba
nikhilsathyanarayana-lab <nikhil.sathyanarayana@pendo.io>
2025-12-23 20:43:17 +0000

    Capture SubID credentials for app selection

--- SPA/js/2.js
+++ SPA/js/2.js
@@ -1,6 +1,5 @@
 import { app_names } from '../API/app.js';
 
 export async function initSection() {
-  const credentialEntries = Array.isArray(window?.appCredentials) ? window.appCredentials : [];
-  app_names(credentialEntries);
+  app_names();
 }
