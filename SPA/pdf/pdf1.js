57211490fe62fb4a4c63dbd4e964dc189bf61061
nikhilsathyanarayana-lab <nikhil.sathyanarayana@pendo.io>
2025-12-31 09:46:42 +0000

    Add full-width duplicate donut chart

--- SPA/pdf/pdf1.js
+++ SPA/pdf/pdf1.js
@@ -1,17 +1,22 @@
-const ctx = document.getElementById('subDonut');
+const chartData = {
+  datasets: [{
+    label: 'My First Dataset',
+    data: [300, 50, 100],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+};
 
-new Chart(ctx, {
+new Chart(document.getElementById('subDonut'), {
   type: 'doughnut',
-  data: {
-    datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4
-    }]
-  }
+  data: chartData
+});
+
+new Chart(document.getElementById('subDonutFull'), {
+  type: 'doughnut',
+  data: chartData
 });
