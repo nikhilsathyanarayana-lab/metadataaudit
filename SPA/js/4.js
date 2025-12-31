6a5a637178fb84e2971d07801751c916474089d9
nikhilsathyanarayana-lab <nikhil.sathyanarayana@pendo.io>
2025-12-31 11:05:27 +0000

    Move sub scan count helper into pdf script

--- SPA/js/4.js
+++ SPA/js/4.js
@@ -1,24 +1,4 @@
-// Read metadata aggregations from the SPA cache.
-const getMetadataAggregations = () => {
-  return typeof window !== 'undefined'
-    ? window.metadataAggregations || {}
-    : {};
-};
-
-// Store the count of unique SubIDs discovered during metadata scans for export consumers.
-export const updateSubScanCount = (aggregations = getMetadataAggregations()) => {
-  const uniqueSubIds = aggregations && typeof aggregations === 'object'
-    ? Object.keys(aggregations)
-    : [];
-
-  const subScanCount = uniqueSubIds.length;
-
-  if (typeof window !== 'undefined') {
-    window.subScanCount = subScanCount;
-  }
-
-  return subScanCount;
-};
+export { updateSubScanCount } from '../pdf/pdf1.js';
 
 // Placeholder initializer for page four of the SPA.
 export async function initSection() {
