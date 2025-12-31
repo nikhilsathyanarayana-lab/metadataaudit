41ff494cc46b8dbef15f339d7ea00b0dac400819
nikhilsathyanarayana-lab <nikhil.sathyanarayana@pendo.io>
2025-12-31 09:49:31 +0000

    Keep PDF donut chart datasets separate

--- SPA/pdf/pdf1.js
+++ SPA/pdf/pdf1.js
@@ -1,4 +1,17 @@
-const chartData = {
+const subDonutData = {
+  datasets: [{
+    label: 'My First Dataset',
+    data: [300, 50, 100],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+};
+
+const subDonutFullData = {
   datasets: [{
     label: 'My First Dataset',
     data: [300, 50, 100],
@@ -13,10 +26,10 @@ const chartData = {
 
 new Chart(document.getElementById('subDonut'), {
   type: 'doughnut',
-  data: chartData
+  data: subDonutData
 });
 
 new Chart(document.getElementById('subDonutFull'), {
   type: 'doughnut',
-  data: chartData
+  data: subDonutFullData
 });
